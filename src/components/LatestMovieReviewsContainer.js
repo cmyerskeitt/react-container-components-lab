ecf5ba01d36e8e572557ac6bc537e5852e6d0024
github username <github email address>
2021-04-03 19:37:40 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,28 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component{
+    constructor(){
+        super()
+        this.state = {
+            reviews: []
+        }
+    }
+
+    componentDidMount(){
+        fetch(URL)
+            .then(response => response.json())
+            .then(lastestReviews => this.setState({reviews: lastestReviews.results}))
+    }
+
+ 
+    render(){
+        return(
+            <div className="latest-movie-reviews">
+                <h2>Lastest Reviews:</h2>
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+}
+export default LatestMovieReviewsContainer
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,33 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const Review = ({
+    headline,
+    link,
+    byline,
+    summary
+}) => {
+    return(
+        <div key={headline} className="review">
+            <header> 
+                <a className="review-link" href={link.url}>
+                    {headline} <b></b>
+                </a>
+               
+                <span className="author">{byline}</span>
+            </header>
+          
+            <blockquote>{summary}</blockquote>
+        </div>
+    )
+}
+
+
+const MovieReviews = ({reviews}) => 
+    <div className='review-list'>
+       {reviews.map(Review)}
+    </div>  
+
+
+
+export default MovieReviews
